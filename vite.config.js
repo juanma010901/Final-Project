ded2121b4c056e5b0fad58d1690693f7802b0a85
Juan M <juanmahc0109@gmail.com>
2024-01-16 16:05:23 -0500

    Frontend deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://juanma010901.github.io/Final-Project',
 })
